35bd09ebd1bf9090f25020fa3479de51b32141bd
MaratVagizov <109537632+MaratVagizov@users.noreply.github.com>
2022-07-21 14:43:19 +0500

    Homework 17
    Homework 17

---
+++ Homework 17.1.js
@@ -0,0 +1,7 @@
+let name = 'Джон'
+let admin = ''
+
+admin = name
+
+
+console.log(admin)
---
+++ Homework 17.2.js
@@ -0,0 +1,9 @@
+let balance = 25000
+let car = 4480
+let food = 890
+let invoice = 5500
+let stock = 4200
+
+totalBalance = balance - car - food + invoice + stock
+
+console.log(totalBalance)
